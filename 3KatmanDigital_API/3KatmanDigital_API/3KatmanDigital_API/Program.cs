5fce7ebdf29347c4a9d54198e98837ec0adb5e8e
btwmeta <137120250+metabtw@users.noreply.github.com>
2025-05-30 06:35:34 +0300

    update

---
+++ 3KatmanDigital_API/3KatmanDigital_API/3KatmanDigital_API/Program.cs
@@ -0,0 +1,69 @@
+using _3KatmanDigital_API.Repository;
+using _3KatmanDigital_API.Repository.Interface;
+using _3KatmanDigital_API.Repository.Repo;
+using _3KatmanDigital_API.Services.Class_Service;
+using _3KatmanDigital_API.Services.Interface;
+using Entitiy;
+using Microsoft.EntityFrameworkCore;
+
+namespace _3KatmanDigital_API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // CORS politikasını ekle
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowFrontend", policy =>
+                {
+                    policy.SetIsOriginAllowed(origin => true) // Geliştirme için tüm originlere izin ver
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowCredentials();
+                });
+            });
+
+            // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+              options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),b=>b.MigrationsAssembly("3KatmanDigital_API")));
+
+            builder.Services.AddScoped(typeof(IGenericRepo<>), typeof(GenericRepo<>));
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
+            builder.Services.AddScoped<IProjectService, ProjectService>();
+            builder.Services.AddScoped<IProjectRequestService, ProjectRequestService>();
+            builder.Services.AddScoped<IService_Service, Service_service>();
+
+            builder.Services.AddScoped<ICategoryRepo, CategoryRepo>();
+            builder.Services.AddScoped<IProjectRepo, ProjectRepo>();
+            builder.Services.AddScoped<IProjectRequestRepo, ProjectRequestRepo>();
+            builder.Services.AddScoped<IServiceRepo, ServiceRepo>();
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // CORS middleware'ini ekle
+            app.UseCors("AllowFrontend");
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            // HTTPS yönlendirmesini kaldır
+            // app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
